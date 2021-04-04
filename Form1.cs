dd2b68ef22011eeec0f57467401488b878471820
Rene Gonzalez <rene_glez1992@hotmail.com>
2021-04-03 23:31:06 -0700

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+/// <summary>
+/// Lenguaje de programacion III
+/// Autor: Rene Gonzalez Rodriguez
+/// Maestro: Aarón I. Salazar
+/// </summary>
+namespace Actividad_Integradora_6_Problema_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
