e253e5d0a73bf2b2224c5b213f0966e2b277c4b6
Rene Gonzalez <rene_glez1992@hotmail.com>
2021-04-03 23:56:41 -0700

    Concluido

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,20 +29,164 @@ namespace Actividad_Integradora_6_Problema_2
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvComponentes = new System.Windows.Forms.DataGridView();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.Componente = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Costo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvComponentes)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(44, 116);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(643, 583);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Olive;
+            this.label1.Location = new System.Drawing.Point(216, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(257, 67);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "René PCs ";
+            // 
+            // dgvComponentes
+            // 
+            this.dgvComponentes.AllowUserToAddRows = false;
+            this.dgvComponentes.AllowUserToDeleteRows = false;
+            this.dgvComponentes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvComponentes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Componente,
+            this.Costo});
+            this.dgvComponentes.Location = new System.Drawing.Point(733, 116);
+            this.dgvComponentes.Name = "dgvComponentes";
+            this.dgvComponentes.ReadOnly = true;
+            this.dgvComponentes.RowHeadersWidth = 102;
+            this.dgvComponentes.RowTemplate.Height = 49;
+            this.dgvComponentes.Size = new System.Drawing.Size(615, 583);
+            this.dgvComponentes.TabIndex = 2;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(1023, 45);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(188, 58);
+            this.btnAgregar.TabIndex = 3;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(851, 51);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(124, 47);
+            this.txtId.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(784, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 41);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Id:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1023, 762);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(217, 41);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Total a Pagar: $";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(1237, 762);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(34, 41);
+            this.lblTotal.TabIndex = 7;
+            this.lblTotal.Text = "0";
+            // 
+            // Componente
+            // 
+            this.Componente.HeaderText = "Componente";
+            this.Componente.MinimumWidth = 12;
+            this.Componente.Name = "Componente";
+            this.Componente.ReadOnly = true;
+            this.Componente.Width = 250;
+            // 
+            // Costo
+            // 
+            this.Costo.HeaderText = "Costo";
+            this.Costo.MinimumWidth = 12;
+            this.Costo.Name = "Costo";
+            this.Costo.ReadOnly = true;
+            this.Costo.Width = 250;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(784, 753);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(188, 58);
+            this.btnCalcular.TabIndex = 8;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1463, 903);
+            this.ClientSize = new System.Drawing.Size(1377, 866);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.dgvComponentes);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Actividad Integradora 6";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvComponentes)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvComponentes;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Componente;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Costo;
+        private System.Windows.Forms.Button btnCalcular;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -9,10 +9,88 @@ namespace Actividad_Integradora_6_Problema_2
 {
     public partial class Form1 : Form
     {
+        int contador = 0;
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            dgvComponentes.Rows.Add(1);
+            int id = Convert.ToInt32(txtId.Text);
+            switch (id)
+            {
+                case 1:
+                    dgvComponentes.Rows[contador].Cells[0].Value = "Targeta Madre";
+                    dgvComponentes.Rows[contador].Cells[1].Value = "1500";
+                    contador++;
+                    break;
+                case 2:
+                    dgvComponentes.Rows[contador].Cells[0].Value = "Procesador";
+                    dgvComponentes.Rows[contador].Cells[1].Value = "3500";
+                    contador++;
+                    break;
+                case 3:
+                    dgvComponentes.Rows[contador].Cells[0].Value = "Memoria Ram 4GB";
+                    dgvComponentes.Rows[contador].Cells[1].Value = "850";
+                    contador++;
+                    break;
+                case 4:
+                    dgvComponentes.Rows[contador].Cells[0].Value = "Memoria Ram 8GB";
+                    dgvComponentes.Rows[contador].Cells[1].Value = "1350";
+                    contador++;
+                    break;
+                case 5:
+                    dgvComponentes.Rows[contador].Cells[0].Value = "Fuente de Poder";
+                    dgvComponentes.Rows[contador].Cells[1].Value = "750";
+                    contador++;
+                    break;
+                case 6:
+                    dgvComponentes.Rows[contador].Cells[0].Value = "Targeta de Video";
+                    dgvComponentes.Rows[contador].Cells[1].Value = "5500";
+                    contador++;
+                    break;
+                case 7:
+                    dgvComponentes.Rows[contador].Cells[0].Value = "SSD";
+                    dgvComponentes.Rows[contador].Cells[1].Value = "1000";
+                    contador++;
+                    break;
+                case 8:
+                    dgvComponentes.Rows[contador].Cells[0].Value = "Disco Duro";
+                    dgvComponentes.Rows[contador].Cells[1].Value = "600";
+                    contador++;
+                    break;
+                case 9:
+                    dgvComponentes.Rows[contador].Cells[0].Value = "Monitor";
+                    dgvComponentes.Rows[contador].Cells[1].Value = "4700";
+                    contador++;
+                    break;
+                case 10:
+                    dgvComponentes.Rows[contador].Cells[0].Value = "Teclado";
+                    dgvComponentes.Rows[contador].Cells[1].Value = "120";
+                    contador++;
+                    break;
+                case 11:
+                    dgvComponentes.Rows[contador].Cells[0].Value = "Mouse";
+                    dgvComponentes.Rows[contador].Cells[1].Value = "90";
+                    contador++;
+                    break;
+                default:
+                    MessageBox.Show("Debe ingresar un Id de la lista", "Aviso");
+                    break;
+            }
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double total = 0;
+            foreach (DataGridViewRow dgvRenglon in dgvComponentes.Rows)
+            {
+                total = total + Convert.ToDouble(dgvRenglon.Cells[1].Value.ToString());
+            }
+
+            lblTotal.Text = total.ToString();
+        }
     }
 }
